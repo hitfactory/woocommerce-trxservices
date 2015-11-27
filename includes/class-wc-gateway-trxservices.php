c3fe3f6f5645643e7fd28bdd8820b5671bcf9d2d
Tami Fenwick <tami@hitfactory.co.nz>
2015-11-27 20:35:14 +1300

    fix ssl check bug

--- includes/class-wc-gateway-trxservices.php
+++ includes/class-wc-gateway-trxservices.php
@@ -185,7 +185,7 @@ class WC_Gateway_TrxServices extends WC_Payment_Gateway {
     }
 
     // Show message if enabled and FORCE SSL is disabled and WordPress HTTPS plugin is not detected.
-    if ( 'no' == get_option( 'woocommerce_force_ssl_checkout' ) && !class_exists( 'WordPressHTTPS' ) ) {
+    if ( 'yes' == get_option( 'woocommerce_force_ssl_checkout' ) && !class_exists( 'WordPressHTTPS' ) ) {
       echo '<div class="error"><p>' . sprintf( __( 'TrxServices is enabled, but the <a href="%s">force SSL option</a> is disabled; your checkout may not be secure! Please enable SSL and ensure your server has a valid SSL certificate - TrxServices will only work in sandbox mode.', 'woocommerce-trxservices'), admin_url( 'admin.php?page=wc-settings&tab=checkout' ) ) . '</p></div>';
     }
   }
